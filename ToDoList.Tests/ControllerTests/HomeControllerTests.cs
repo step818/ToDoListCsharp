34cdcfd65d537e0415ff62164b6f2e6e1ee328d5
Student <example@example.com>
2019-04-29 16:43:07 -0700

    add a test

--- ToDoList.Tests/ControllerTests/HomeControllerTests.cs
+++ ToDoList.Tests/ControllerTests/HomeControllerTests.cs
@@ -9,6 +9,17 @@ namespace ToDoList.Tests
     [TestClass]
     public class HomeControllerTest
     {
+      [TestMethod]
+      public void Index_ReturnsCorrectView_True()
+      {
+          //Arrange
+          HomeController controller = new HomeController();
 
+          //Act
+          ActionResult indexView = controller.Index();
+
+          //Assert
+          Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+      }
     }
 }
