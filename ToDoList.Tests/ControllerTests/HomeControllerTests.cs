e440983e89792858830104a528eae2bcb5f93caa
Student <example@example.com>
2019-04-29 16:28:13 -0700

    add test controllers

---
+++ ToDoList.Tests/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToDoList.Controllers;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+
+    }
+}
