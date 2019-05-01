d5d672c9a379898ad20b385d6df0ba1db04300dc
Kyle Harrington <harrington.kyle@gmail.com>
2019-04-30 20:51:32 -0700

    add a delete all page + function, and a show all page. change CreateForm to New to match RESTful conventions

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -81,5 +81,33 @@ namespace ToDoList.Tests
       CollectionAssert.AreEqual(newList, result);
     }
 
+    [TestMethod]
+    public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      int result = newItem.GetId();
+
+      //Assert
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectItem_Item()
+    {
+      // Arrange
+      string description01 = "walk the dog";
+      string description02 = "do the laundry";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item (description02);
+      // Act
+      Item result = Item.Find(1);
+      // Assert
+      Assert.AreEqual(newItem1, result);
+    }
+
   }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,12 +5,14 @@ namespace ToDoList.Models
   public class Item
   {
     private string _description;
+    private int _id;
     private static List<Item> _instances = new List<Item> {};
 
     public Item (string description)
     {
       _description = description;
       _instances.Add(this);
+      _id = _instances.Count;
     }
 
     public string GetDescription()
@@ -31,6 +33,16 @@ namespace ToDoList.Models
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public static Item Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
 
   }
